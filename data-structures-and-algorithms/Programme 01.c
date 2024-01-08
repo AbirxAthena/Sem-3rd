685c22cebee62172bca9d4aed1af800ced1f6ffe
AbirxAthena <142162873+AbirxAthena@users.noreply.github.com>
2024-01-08 23:26:17 +0530

    Create Programme 01.c

---
+++ data-structures-and-algorithms/Programme 01.c
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+int main () 
+{
+int x, y;
+
+int sum;
+
+sum = x+y;
+
+cout << "Enter two integers" << endl;
+
+cin >> x >> y ;
+
+cout << "sum of x and y is: " << x+y << endl;
+return 0;
+}
+
+
+
+//Output:
+//Enter 10 numbers:
+//Number 1: 10
+//Number 2: 20
+//Number 3: 30
+//Number 4: 40
+//Number 5: 50
+//Number 6: 60
+//Number 7: 70
+//Number 8: 80
+//Number 9: 90
+//Number 10: 100  1
+
+//Printing the numbers:
+//10 20 30 40 50 60 70 80 90 11
