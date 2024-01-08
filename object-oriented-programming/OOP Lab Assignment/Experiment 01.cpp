eb6ccb1586b795a9b454a7272d4b552ddda2c024
AbirxAthena <142162873+AbirxAthena@users.noreply.github.com>
2024-01-08 21:31:16 +0530

    Create Experiment 01.cpp

---
+++ object-oriented-programming/OOP Lab Assignment/Experiment 01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std
+
+int main () 
+{
+int x, y ;
+
+int sum;
+
+Sum = x+y;
+
+cout << "Enter two integers" << endl;
+
+cin >> x >> y ;
+
+cout << "sum of x and y is: " << x+y << endl;
+return 0;
+}
