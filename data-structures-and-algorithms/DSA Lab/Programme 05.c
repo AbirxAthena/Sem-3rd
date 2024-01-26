424095f80c5b8201fe5b8d514b9217b1298c1020
Abir Gupta <142162873+abirxgpt@users.noreply.github.com>
2024-01-26 15:45:55 +0530

    Update Programme 05.c

--- data-structures-and-algorithms/DSA Lab/Programme 05.c
+++ data-structures-and-algorithms/DSA Lab/Programme 05.c
@@ -1,58 +1,54 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <conio.h>
 
-int compare(const void *a, const void *b) {
-    return (*(int *)a - *(int *)b);
-}
+    void main() {
+    int a[100];
+    int i,j,sum,size,mm;
+    float mean,median,t;
+    printf("Enter size of the array");
+    scanf("%d",&size);
+    
+    printf("Enter %d numbers:\n",size);
 
-float calculateMean(int arr[], int size) {
-    int sum = 0;
-    for (int i = 0; i < size; ++i) {
-        sum += arr[i];
-    }
-    return (float)sum / size;
-}
-
-float calculateMedian(int arr[], int size) {
-    if (size % 2 == 0) {
-        return (float)(arr[size / 2 - 1] + arr[size / 2]) / 2;
-    } else {
-        return arr[size / 2];
-    }
-}
-
-int main() {
-    int size;
-    printf("Enter the size of the array: ");
-    scanf("%d", &size);
-
-    int arr[size];
-
-    printf("Enter %d numbers:\n", size);
-    for (int i = 0; i < size; ++i) {
-        printf("Element %d: ", i + 1);
-        scanf("%d", &arr[i]);
+    for (i=0; i<size ; i++)
+    {
+        scanf("%d", &a[i]);
     }
 
-    qsort(arr, size, sizeof(int), compare);
-
-    printf("Mean: %.2f\n", calculateMean(arr, size));
-    printf("Median: %.2f\n", calculateMedian(arr, size));
-
-    return 0;
-}
-
-
-
-
-//Output:
-//Enter the size of the array: 6
-//Enter 6 numbers:
-//Element 1: 10
-//Element 2: 2
-//Element 3: 30
-//Element 4: 40
-//Element 5: 50
-//Element 6: 60
-//Mean: 32.00
-//Median: 35.00
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size -1 ; j++){
+            if(a[j] > a[j+1])
+            {
+                t=a[j];
+                a[j]=a[j+1];
+                a[j+1]=t;
+            }
+        }
+    }
+     for (i = 0; i < size; i++) {
+         sum+=a[i];
+     }
+    
+ 
+        printf("Sorted Array of Elements are:");
+        for (i=0; i<size; ++i)
+        {
+            printf("%d \n",a[i]);
+        }
+        if((size%2)==0)
+        {
+         mm = size/2;
+         t = a[mm]+a[mm - 1];
+         median = t/2;
+        }
+        else
+        {
+        mm=size/2;
+        median = a[mm];
+        }
+        mean = sum/size;
+        printf("Mean of the array is: %f \n & Median of array is: %f",mean,median);
+        
+    
+    getch();
+    }
