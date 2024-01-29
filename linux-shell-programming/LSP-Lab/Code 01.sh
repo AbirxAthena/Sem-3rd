f5f73a63be13ee03bade8c5aa22f1359ffd68d90
Abir Gupta <142162873+abirxgpt@users.noreply.github.com>
2024-01-29 22:53:29 +0530

    Create Code 01.sh

---
+++ linux-shell-programming/LSP-Lab/Code 01.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Enter three numbers"
+read num1
+read num2
+read num3
+
+if [ $num1 -gt $num2 ] && [ $num1 -gt $num3 ]; then
+    echo "First Number $num1 is the greatest!"
+elif [ $num2 -gt $num1 ] && [ $num2 -gt $num3 ]; then
+    echo "Second Number $num2 is the greatest!"
+else
+    echo "Third Number $num3 is the greatest!"
+fi
